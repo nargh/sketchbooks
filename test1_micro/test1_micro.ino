bcb2fcd4152564eae9d01550969d5430c49fc86a
Kusterer <ga73coh@daedalus20.eikon.tum.de>
2014-06-01 17:29:01 +0200

    höhenregelung

---
+++ test1_micro/test1_micro.ino
@@ -0,0 +1,466 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU9150
+// 1/4/2013 original by Jeff Rowberg <jeff@rowberg.net> at https://github.com/jrowberg/i2cdevlib
+// modified by Aaron Weiss <aaron@sparkfun.com>
+//
+// Changelog:
+// 2011-10-07 - initial release
+// 2013-1-4 - added raw magnetometer output
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#include "Wire.h"
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro(0x69);
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int16_t mx, my, mz;
+
+#define LED_PIN 13
+bool blinkState = false;
+
+
+
+
+
+
+//Base station v0.9.1 20130704 1300***************************************************
+#define VERSION "v0.9"
+#define DEVICEID 0
+
+#define VERSIONMAJOR 0
+#define VERSIONMINOR 9
+
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>0
+#include <MirfHardwareSpiDriver.h>
+
+// NRF24 settings
+#define RFADDR "alex1"
+#define RFBASE "alex0"
+#define RFCHANNEL 3
+
+//constants
+const byte MYID = DEVICEID;
+const byte Version[2] = {VERSIONMAJOR,VERSIONMINOR};
+const byte MAXSTATIONS = 15;
+
+// Global Variables
+int PID = 5;
+
+class Packet {
+  //byte time;
+  unsigned long time;
+  byte sent;
+  byte data[12];
+  
+  public:
+    Packet (byte dest, byte type, byte *payload) {
+      sent = 0;
+      //time = millis() % 128;
+      time = millis();
+      data[0] = dest;
+      data[1] = MYID;
+      data[2] = type;
+      data[3] = (byte)((PID & 0xFF00) >> 8);
+      data[4] = (byte)(PID & 0x00FF);
+      PID++;
+      for (int i = 0; i<7;i++){
+        data[i+5] = payload[i];
+      }
+    }
+    ~Packet () {
+    }
+    byte send (void){
+      if(millis() - time < data[2]) {
+        if (millis()-time >= sent*5){
+          data[11] = sent;
+          Mirf.setTADDR((byte *)RFBASE);
+          Mirf.send(data);
+          sent++;
+        }
+        if(data[2] == 192) return 1;
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+    
+    unsigned int getPID() {
+      return (unsigned int)((unsigned int)data[3]<< 8 + (unsigned int)data[4]);
+    }
+};
+Packet *Packages[5];
+byte busy=0;
+int devicePid [MAXSTATIONS][5] = {{0}};
+int deviceLastPid [MAXSTATIONS] = {0};
+
+void sendPackages(void) {
+    if ((busy&1)) {
+      if (Packages[0]->send()) {
+        busy &= 0b11111110;
+        delete Packages[0];
+      }
+    } else if ((busy&2)) {
+      if (Packages[1]->send()) {
+        busy &= 0b11111101;
+        delete Packages[1];
+      }
+    } else if ((busy&4)) {
+      if (Packages[2]->send()) {
+        busy &= 0b11111011;
+        delete Packages[2];
+      }
+    } else if ((busy&8)) {
+      if (Packages[3]->send()) {
+        busy &= 0b11110111;
+        delete Packages[3];
+      }
+    } else if ((busy&16)) {
+      if (Packages[4]->send()) {
+        busy &= 0b11101111;
+        delete Packages[4];
+      }
+    }
+}
+boolean newPacket (byte dest, byte type, byte *payload) {
+  if (busy<31){
+    if (!(busy&1)) {
+      Packages[0] = new Packet (dest, type,payload);
+      busy |= 1;
+    } else if (!(busy&2)) {
+      Packages[1] = new Packet (dest, type,payload);
+      busy |= 2;
+    } else if (!(busy&4)) {
+      Packages[2] = new Packet (dest, type,payload);
+      busy |= 4;
+    } else if (!(busy&8)) {
+      Packages[3] = new Packet (dest, type,payload);
+      busy |= 8;
+    } else if (!(busy&16)) {
+      Packages[4] = new Packet (dest, type,payload);
+      busy |= 16;
+    }
+    return true;
+  } else return false;
+}
+
+void deletePID (int pid) {
+  Serial.println(busy);
+    if ((busy&1)) {
+      if (Packages[0]->getPID() == pid) {
+        busy &= 0b11111110;
+        delete Packages[0];
+      }
+    } else if ((busy&2)) {
+      if (Packages[1]->getPID() == pid) {
+        busy &= 0b11111101;
+        delete Packages[1];
+      }
+    } else if ((busy&4)) {
+      if (Packages[2]->getPID() == pid) {
+        busy &= 0b11111011;
+        delete Packages[2];
+      }
+    } else if ((busy&8)) {
+      if (Packages[3]->getPID() == pid) {
+        busy &= 0b11110111;
+        delete Packages[3];
+      }
+    } else if ((busy&16)) {
+      if (Packages[4]->getPID() == pid) {
+        busy &= 0b11101111;
+        delete Packages[4];
+      }
+    }
+    Serial.println(busy);
+}
+byte parseMsg() {
+  byte data[12];
+  Mirf.getData(data);
+  unsigned int pid;
+  pid = ((((unsigned int)data[3])<< 8) + (unsigned int)data[4]);
+  byte from = data[1];
+  
+  if(data[0] == MYID ) {
+    if (isNewPid(from,pid)) {
+      switch(data[2]) {
+        case 192: //ACK
+          unsigned int ackpid;
+          ackpid = ((unsigned int)data[5]<< 8 + (unsigned int)data[6]);
+          deletePID(ackpid);
+          Serial.print("t");
+          break;
+        case 193: {//REG
+          byte pid[2];
+          pid[0] = data[3];
+          pid[1] = data[4];
+          byte from = data[1];
+          newPacket(from, (byte)192, pid);
+          }
+          break;
+        case 64: {//deltat
+          unsigned long deltat;
+          deltat = (unsigned long)data[8];
+          deltat += (unsigned long)data[7]<<8;
+          deltat += (unsigned long)data[6]<<16;
+          deltat += (unsigned long)data[5]<<24;
+          byte from = data[1];
+          sync(from,data[9]);
+          Serial.print("t");
+          if(from<10) Serial.print('0');
+          Serial.print(from);
+          if(deltat<10) Serial.print('0');
+          if(deltat<100) Serial.print('0');
+          if(deltat<1000) Serial.print('0');
+          if(deltat<10000) Serial.print('0');
+          if(deltat<100000) Serial.print('0');
+          if(deltat<1000000) Serial.print('0');
+          if(deltat<10000000) Serial.print('0');
+          if(deltat<100000000) Serial.print('0');
+          if(deltat<1000000000) Serial.print('0');
+          Serial.print(deltat);
+          Serial.print("\n");
+          byte pid[2];
+          pid[0] = data[3];
+          pid[1] = data[4];
+          
+          
+          newPacket(from, (byte)192, pid);
+          }
+          break;
+          
+          case 101: {
+            int16_t x= (data[5]<<8) + data[6];
+            int16_t y= (data[7]<<8) + data[8];
+            int16_t z= (data[9]<<8) + data[10];
+            Serial.print("aX: ");Serial.print(x);
+            Serial.print("aY: ");Serial.print(y);
+            Serial.print("aZ: ");Serial.print(z);
+          }
+          break;
+            case 102: {
+            int16_t x= (data[5]<<8) + data[6];
+            int16_t y= (data[7]<<8) + data[8];
+            int16_t z= (data[9]<<8) + data[10];
+            Serial.print("gX: ");Serial.print(x);
+            Serial.print("gY: ");Serial.print(y);
+            Serial.print("gZ: ");Serial.print(z);
+          }
+          break;
+            case 103: {
+            int16_t x= (data[5]<<8) + data[6];
+            int16_t y= (data[7]<<8) + data[8];
+            int16_t z= (data[9]<<8) + data[10];
+            Serial.print("mX: ");Serial.print(x);
+            Serial.print("mY: ");Serial.print(y);
+            Serial.print("mZ: ");Serial.print(z);
+          }
+          break;
+          
+        default:
+          return 0; //kenne Datentyp nicht
+      }
+    } else {// Paket wurde bereits empfangen, wird nicht verarbeitet sondern nur bestätigt
+      byte pid[2];
+      pid[0] = data[3];
+      pid[1] = data[4];
+      newPacket(from, (byte)192, pid);
+    }
+  } else {
+    return 0; //Nachricht nicht für mich
+  }
+  return 1;
+}
+
+// Prüft ob PID neu oder bereits empfangen wurde.
+// Rückgabewert 0, falls bereits empfangen
+// 1, falls neue PID
+byte isNewPid(byte from, unsigned int pid) {
+  unsigned int minpid = 65535;
+  byte minpidi =0;
+  for(byte i = 0 ; i<5 ; i++) {
+    if(devicePid[from][i] == pid) {
+      return 0; //PID bereits empfangen
+    }
+    if(devicePid[from][i] < minpid) {
+      minpid = devicePid[from][i];
+      minpidi = i;
+    }
+  }
+  if (minpid < pid) {
+    devicePid[from][minpidi] = pid;
+    deviceLastPid[from] = pid;
+    return 1;
+  } else if (pid <15) {
+    for(byte i = 0 ; i<5 ; i++) {
+      if(devicePid[from][i] > 15) {
+        devicePid[from][i] = 0;
+        minpidi = i;
+      }
+    }
+    devicePid[from][minpidi] = pid;
+    deviceLastPid[from] = pid;
+    return 1;
+  } else if (deviceLastPid[from] < minpid && deviceLastPid[from] != pid){
+    devicePid[from][minpidi] = pid;
+    deviceLastPid[from] = pid;
+    return 1;
+  }
+  deviceLastPid[from] = pid;
+  return 2;
+}
+
+
+byte devicesync[MAXSTATIONS] = {0};
+// sync
+void sync(byte from, byte syn) {
+  if (devicesync[from]) {
+    Serial.println("sync");
+    for (int i=0;i<MAXSTATIONS;i++) {
+      devicesync[i] = 0;
+    }
+  }
+  devicesync[from] = 1;
+}
+
+//**************************************************************************
+
+
+
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    Wire.begin();
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(38400);
+delay(10000);
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+    // configure Arduino LED for
+    pinMode(LED_PIN, OUTPUT);
+    
+  // senden********************  
+    
+  Serial.begin(115200);
+  Serial.print("IPS ");
+  Serial.println(VERSION);
+  //init NRF24
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.cePin = 9;
+  Mirf.csnPin = 10;
+  Mirf.init();
+  Mirf.setRADDR((byte *)RFADDR);
+  Mirf.payload = 12;
+  Mirf.channel = RFCHANNEL;
+  Mirf.config();
+  
+  //PID = readPID();
+  
+  //newPacket((byte)1, (byte)193, Version);
+  
+}
+
+void loop() {
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    // display tab-separated accel/gyro x/y/z values
+    
+    Serial.print("a/g/m:\t");
+    Serial.print(ax); Serial.print("\t");
+    Serial.print(ay); Serial.print("\t");
+    Serial.print(az); Serial.print("\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.print(gz); Serial.print("\t");
+    Serial.print(mx); Serial.print("\t");
+    Serial.print(my); Serial.print("\t");
+    Serial.println(mz);
+// This example code is in the public domain.
+
+// <http://www.zambetti.com> 
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+    
+    
+    //senden***************************
+    
+    byte a[6], g[6], m[6];
+    a[0]= (ax & 0xFF00) >> 8;a[1]= (ax & 0x00FF);
+    a[2]= (ay & 0xFF00) >> 8;a[3]= (ay & 0x00FF);
+    a[4]= (az & 0xFF00) >> 8;a[5]= (az & 0x00FF);
+    
+    g[0]= (gx & 0xFF00) >> 8;g[1]= (gx & 0x00FF);
+    g[2]= (gy & 0xFF00) >> 8;g[3]= (gy & 0x00FF);
+    g[4]= (gz & 0xFF00) >> 8;g[5]= (gz & 0x00FF);
+    
+    m[0]= (mx & 0xFF00) >> 8;m[1]= (mx & 0x00FF);
+    m[2]= (my & 0xFF00) >> 8;m[3]= (my & 0x00FF);
+    m[4]= (mz & 0xFF00) >> 8;m[5]= (mz & 0x00FF);
+    
+    sendPackages();
+    if (newPacket (55, 101, a) ) {
+      Serial.println("a1");
+    } else {
+      Serial.println("a0");
+    }
+    sendPackages();
+    if (newPacket (55, 102, g)) Serial.println("g1");
+    sendPackages();
+    if (newPacket (55, 103, m)) Serial.println("m1");
+    
+    sendPackages();
+  while(Mirf.isSending()) {Serial.println("s");};
+  if(Mirf.dataReady()){
+    parseMsg();
+  }
+}
